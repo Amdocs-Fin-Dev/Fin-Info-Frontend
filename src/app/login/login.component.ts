2de311efee4f87e8caacc3d207b01125426e3fd9
Alvaro Felix Guzman <okaru.alvaro97@gmail.com>
2021-08-10 15:14:37 -0500

    added login component

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { SocialAuthService, GoogleLoginProvider } from 'angularx-social-login';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor(
+    private router:Router,
+    private authService: SocialAuthService
+  ) { }
+
+  ngOnInit(): void {
+  } 
+
+  signInHandle():void{
+    //Manejamos toda la autenticacion y todo lo pasamos en el Key 'google_auth'
+    this.authService.signIn(GoogleLoginProvider.PROVIDER_ID).then((data)=>{
+      localStorage.setItem('google_auth', JSON.stringify(data));
+      console.log(data);
+      //redireccionamos la url al dashboard donde esta el componente dashboard
+      this.router.navigateByUrl('/dashboard').then();
+      // process.env.GOOGLE_ID;
+      // console.log('Este es el ID');
+      // console.log(process.env.GOOGLE_ID);
+    });
+  }
+
+  
+}
