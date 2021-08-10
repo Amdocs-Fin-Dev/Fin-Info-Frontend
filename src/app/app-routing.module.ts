c7b76af402e902c3db5e9f44a8b05cebf6bd277f
Alvaro Felix Guzman <okaru.alvaro97@gmail.com>
2021-08-10 15:09:12 -0500

    Added dashboard and login routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,26 @@ import { RouterModule, Routes } from '@angular/router';
 import { ChartComponent } from './chart/chart.component';
 import { LineChartComponent } from './chart/line-chart/line-chart.component';
 import { CandleChartComponent } from './chart/candle-chart/candle-chart.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
 const routes: Routes = [
   {path: 'mostrar/', component: ChartComponent},
   {path: 'line', component: LineChartComponent},
   {path: 'candle', component: CandleChartComponent},
+  // {
+  //   path: '',
+  //   redirectTo: 'login',
+  //   pathMatch: 'full'
+  // },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path:'dashboard',
+    component: DashboardComponent
+  }
 ];
 
 @NgModule({
