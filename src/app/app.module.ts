4c268de8c1943919514aaedc4f2145d2d65bafe8
Alvaro Felix Guzman <okaru.alvaro97@gmail.com>
2021-08-10 15:11:54 -0500

    Added modules and providers for login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,23 +11,47 @@ import {HttpClientModule} from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { LineChartComponent } from './chart/line-chart/line-chart.component';
 import { CandleChartComponent } from './chart/candle-chart/candle-chart.component';
+import { GoogleLoginProvider, SocialAuthServiceConfig, SocialLoginModule } from 'angularx-social-login';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ChartComponent,
     LineChartComponent,
-    CandleChartComponent
+    CandleChartComponent,
+    LoginComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
+    SocialLoginModule,
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
     GoogleChartsModule
   ],
-  providers: [SharedService],
+  providers: [
+    SharedService,
+    {
+      provide: 'SocialAuthServiceConfig',
+      useValue: {
+        autoLogin: false,
+        providers:[
+          {
+            id: GoogleLoginProvider.PROVIDER_ID,
+            provider: new GoogleLoginProvider(
+              '945529565086-5dv1q0mh9po4hgjlq3rvse054fj19e7t.apps.googleusercontent.com'
+            )
+          }
+        ]
+      } as SocialAuthServiceConfig
+    }
+  
+  
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
