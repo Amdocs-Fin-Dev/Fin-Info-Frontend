7cdb7a5c89559dfcb52ae5849b850805182b92c1
Alvaro Felix Guzman <okaru.alvaro97@gmail.com>
2021-08-22 20:07:02 -0500

    Added delete method to services

--- src/app/shared.service.ts
+++ src/app/shared.service.ts
@@ -12,21 +12,22 @@ export class SharedService {
 
 readonly APIUrl = "http://127.0.0.1:8000/";
 
+  public portafolio: any = [];
   data: any = [];
   public ChartList: any = [];
 
   constructor(private http: HttpClient) { }
   
-  getDepListTest(ticker_id: string, interval: string):Observable<any[]>{
-    console.log(ticker_id);
-    console.log(interval);
-    this.data = this.http.get<any[]>(this.APIUrl + 'mostrar/' + ticker_id + '/' + interval);
-    console.log("Datos",this.data);
-    return this.http.get<any[]>(this.APIUrl + 'mostrar/' + ticker_id + '/' + interval);
+  getDepListTest(ticker_id: string, interval: string, period:string):Observable<any[]>{
+    // console.log(ticker_id);
+    // console.log(interval);
+    this.data = this.http.get<any[]>(this.APIUrl + 'mostrar/' + ticker_id + '/' + interval + '/' + period);
+    // console.log("Datos",this.data);
+    return this.http.get<any[]>(this.APIUrl + 'mostrar/' + ticker_id + '/' + interval  + '/' + period);
   }
 
-  getData(ticker_id: string, interval: string){
-    this.http.get(this.APIUrl + 'mostrar/' + ticker_id + '/' + interval).subscribe(resp =>{
+  getData(ticker_id: string, interval: string, period:string){
+    this.http.get(this.APIUrl + 'mostrar/' + ticker_id + '/' + interval  + '/' + period).subscribe(resp =>{
       // console.log(resp);
       this.ChartList = resp;
       this.ChartList = JSON.parse(this.ChartList);
@@ -38,21 +39,37 @@ readonly APIUrl = "http://127.0.0.1:8000/";
       return this.ChartList;
   }
 
-//For Dashboard
+//For Dashboard and accounts 
+//Using account as first route
   getUserList():Observable<any[]>{
-    this.data = this.http.get<any[]>(this.APIUrl + 'api/' );
-    console.log("Datos",this.data);
-    return this.http.get<any[]>(this.APIUrl + 'api/');
+    this.data = this.http.get<any[]>(this.APIUrl + 'account/api/' );
+    // console.log("Datos",this.data);
+    return this.http.get<any[]>(this.APIUrl + 'account/api/');
   }
 
   addUser(val:any){
-    return this.http.post<any>(this.APIUrl + 'api/', val);
+    return this.http.post<any>(this.APIUrl + 'account/api/', val);
   }
 
   getPorfolio(email: string):Observable<any[]>{
-    this.data = this.http.get<any[]>(this.APIUrl + 'portfolio/' + email);
-    return this.data = this.http.get<any[]>(this.APIUrl + 'portfolio/' + email);
+    this.data = this.http.get<any[]>(this.APIUrl + 'account/portfolio/' + email +'/');
+    return this.data = this.http.get<any[]>(this.APIUrl + 'account/portfolio/' + email);
 
+  }
+
+  addPortfolio(val:any){
+    return this.http.post<any>(this.APIUrl+ 'account/addfavorites/',val);
+  }
+
+  deletePortfolio(email:string, ticker: string){
+    return this.http.delete<any>(this.APIUrl + 'account/portfolio/' + email + '/' + ticker);
+  }
+
+  buscarQuery(query: string){
+    this.getPorfolio('aklivmairkoo@gmail.com').subscribe(res=>{
+      this.portafolio = res;
+      console.log("nuevo port: ",this.portafolio);
+    });
   }
 
 }
