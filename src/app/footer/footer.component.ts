204d79869a34c22de877b226d143ec8dec0d5d77
Alvaro Felix Guzman <okaru.alvaro97@gmail.com>
2021-09-02 16:54:02 -0500

    Added footer page

---
+++ src/app/footer/footer.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
+// MDB Angular Free
+import { WavesModule, ButtonsModule, IconsModule } from 'angular-bootstrap-md'
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
+})
+export class FooterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+
+}
