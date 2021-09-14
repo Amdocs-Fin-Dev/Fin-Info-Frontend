f662016c9dfa9625355694f72c17104ef4101302
Alvaro Felix Guzman <okaru.alvaro97@gmail.com>
2021-09-14 02:45:30 -0500

    Added more interactivity and new charts

--- src/app/analysis/chart-analysis/chart-analysis.component.ts
+++ src/app/analysis/chart-analysis/chart-analysis.component.ts
@@ -2,6 +2,7 @@ import { AfterViewInit, Component, OnInit, ViewChild, ElementRef } from '@angula
 
 import { SharedService } from '../../shared.service';
 declare var google: any;
+declare var google1: any;
 @Component({
   selector: 'app-chart-analysis',
   templateUrl: './chart-analysis.component.html',
@@ -16,16 +17,24 @@ export class ChartAnalysisComponent implements OnInit, AfterViewInit {
     let rsi = [];
     let fecha = [];
     let close = [];
+    let high = [];
+    let low = [];
+    let open = [];
     let stOscillator = [];
     let adx = [];
     let AvgTrueRange = [];
     let dates:Array<Date> = [];
+
     data.addColumn('datetime', 'Date');
-    data.addColumn('number', 'RSI');
     data.addColumn('number', 'Close');
-    data.addColumn('number', 'stOscillator');
-    data.addColumn('number', 'adx');
-    data.addColumn('number', 'AvgTrueRange');
+    data.addColumn('number', 'High');
+    data.addColumn('number', 'Low');
+    data.addColumn('number', 'Open');
+    data.addColumn('number', 'Close');
+    // data.addColumn('number', 'stOscillator');
+    // data.addColumn('number', 'RSI');
+    // data.addColumn('number', 'adx');
+    // data.addColumn('number', 'AvgTrueRange');
 
     // for(var i = 0; i < this.dataAnalisis.length; i++){
     //   data.addRows([[this.dataAnalisis.Date[i], this.dataAnalisis.rsi[i]]]);
@@ -50,6 +59,9 @@ export class ChartAnalysisComponent implements OnInit, AfterViewInit {
     rsi = this.dataAnalisis.rsi;
     fecha = this.dataAnalisis.Date;
     close = this.dataAnalisis.Close;
+    high = this.dataAnalisis.High;
+    low = this.dataAnalisis.Low;
+    open = this.dataAnalisis.Open;
     stOscillator = this.dataAnalisis.stOscillator;
     adx = this.dataAnalisis.adx;
     AvgTrueRange = this.dataAnalisis.AvgTrueRange;
@@ -57,33 +69,130 @@ export class ChartAnalysisComponent implements OnInit, AfterViewInit {
     // console.log("RSI",this.dataAnalisis.rsi);
     for(var i = 0; i < index; i++){
       console.log("valores",rsi[i]);
-      data.addRows([[dates[i], rsi[i],close[i], stOscillator[i],adx[i],AvgTrueRange[i]]]);
+      data.addRows([[dates[i], high[i],open[i],close[i],low[i],close[i]]]);
     }
+    // rsi[i],stOscillator[i],adx[i],AvgTrueRange[i]
     const options = {
       hAxis: {
         // title: 'Date'
-
+      
       },
       vAxis: {
         // title: 'Average'
+        
       },
       // backgroundColor: '#eef2eb',
-      chartArea: {'width': '90%', 'height': '80%'},
+      chartArea: {'width': '100%', 'height': '100%'},
                'legend': {'position': 'bottom'},
+      // legend: 'series',
       // width: 1000,
       // height: 900,
+      enableInteractivity:true,
+      // bar: { groupWidth: '60%' },
+      candlestick: {
+        fallingColor: { strokeWidth: 0, fill: '#a52714' }, // red
+        risingColor: { strokeWidth: 0, fill: '#0f9d58' }   // green
+      },
+      animation: {
+        duration: 2000,
+        easing: 'linear',
+        startup: true
+      },
+      seriesType: 'candlesticks',
       series: {
-        0: { color: '#3fcfd4' }
+        1: { type:'line',
+              hAxis: {scaleType: 'mirrorLog' }},
+        2: { type:'line'},
+        3: { type:'line'},
       },
       explorer: { 
         //actions: ['dragToZoom', 'rightClickToReset'],
         axis: 'horizontal',
-        maxZoomIn: 100,
-        maxZoomOut: 70
+        maxZoomIn: 10,
+        maxZoomOut: 7
         }
     };
 
-    const chart = new google.visualization.LineChart(this.rsiChart.nativeElement);
+    // const chart = new google.visualization.LineChart(this.rsiChart.nativeElement);
+    const chart = new google.visualization.ComboChart(this.rsiChart.nativeElement);
+    chart.draw(data, options);
+  }
+
+  //------------------------------------------------------------------------------------------
+  @ViewChild('lineChart') lineChart!: ElementRef;
+  draw2 = (list: any) =>{
+    list = this.dataAnalisis;
+    const data = new google.visualization.DataTable();
+    let rsi = [];
+    let fecha = [];
+    let close = [];
+    let stOscillator = [];
+    let adx = [];
+    let AvgTrueRange = [];
+    let dates:Array<Date> = [];
+
+    data.addColumn('datetime', 'Date');
+    data.addColumn('number', 'RSI');
+    data.addColumn('number', 'stOscillator');
+    data.addColumn('number', 'adx');
+    data.addColumn('number', 'AvgTrueRange');
+    var array:Array<string> = Object.values(this.dataAnalisis.Date);
+     //Convertimos nuestro array de objectos con las fechas en epoch en enteros
+     for (let i = 0; i < array.length; i++) {
+      //Convertir los datos del objeto en enteros (es tipo object no string)
+      const intValor = parseInt(array[i]);
+      //console.log("Prueba=", intValor);
+
+      //pasamos las fechas con la funcion date para que los valores en enteros puedan ser Datetime
+      var new_date = new Date(intValor);
+      // console.log(new_date);
+
+      //recoger todas las fechas formateadas a Datetime.
+      dates.push(new_date);
+    }
+    const index = Object.keys(this.dataAnalisis.Date).length;
+    console.log("Indices :3", index);
+    rsi = this.dataAnalisis.rsi;
+    fecha = this.dataAnalisis.Date;
+    close = this.dataAnalisis.Close;
+    stOscillator = this.dataAnalisis.stOscillator;
+    adx = this.dataAnalisis.adx;
+    AvgTrueRange = this.dataAnalisis.AvgTrueRange;
+    for(var i = 0; i < index; i++){
+      console.log("valores",rsi[i]);
+      data.addRows([[dates[i],close[i],rsi[i],
+        AvgTrueRange[i],adx[i]]]);
+    }
+
+    const options = {
+      hAxis: {
+        title: 'Tecnichal Indicators'
+
+      },
+      vAxis: {
+        // title: 'Average'
+        
+      },
+      chartArea: {'width': '100%', 'height': '100%'},
+      // backgroundColor: '#eef2eb',
+      'legend': {'position': 'bottom'},
+      lineWidth: 2,
+      animation: {
+        duration: 1500,
+        easing: 'linear',
+        startup: true
+      },
+      series: {
+        // 0: { color: '#3fcfd4' }
+      },
+      explorer: { 
+        //actions: ['dragToZoom', 'rightClickToReset'],
+        axis: 'horizontal',
+        maxZoomIn: 10,
+        maxZoomOut: 7
+        }
+    };
+    const chart = new google.visualization.LineChart(this.lineChart.nativeElement);
     chart.draw(data, options);
   }
 
@@ -103,9 +212,13 @@ export class ChartAnalysisComponent implements OnInit, AfterViewInit {
       this.dataAnalisis = data;
       this.dataAnalisis = JSON.parse(this.dataAnalisis);
       const indice = this.dataAnalisis["rsi"];
-      // console.log(indice);
-      google.charts.load('current', {packages: ['corechart', 'line']});
+      console.log("Open",this.dataAnalisis.Open);
+      // google.charts.load('current', {packages: ['corechart', 'line']});
+      google.charts.load('current', {packages: ['corechart']});
       google.charts.setOnLoadCallback(this.drawChart);
+
+      google.charts.load('current', {packages: ['corechart', 'line']});
+      google.charts.setOnLoadCallback(this.draw2);
     });
   }
 }
